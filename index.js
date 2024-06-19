261bc8264d9dcfe71db49f17e04bfe265e7e32ed
MD. SOHAN MILLAT SAKIB <112830039+MillatSakib@users.noreply.github.com>
2024-06-19 20:33:38 +0600

    Unnecessary Code Removed

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const sysOs = osu.os;
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-    res.send("Hello")
-})
-
 app.get('/', async (req, res) => {
     // Get total memory in bytes
     const totalMemory = os.totalmem();
